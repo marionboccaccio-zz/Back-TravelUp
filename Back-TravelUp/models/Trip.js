f70f3a3e0377df67b40c81291d9717135f959e88
marionboccaccio <marionboccaccio@MacBook-Air-de-Marion.local>
2019-12-05 13:05:26 +0100

    add trip schema

--- Back-TravelUp/models/Trip.js
+++ Back-TravelUp/models/Trip.js
@@ -1,27 +1,2 @@
-const mongoose = require("mongoose");
-
+const mongoose = require("mongoose"); 
 const Schema = mongoose.Schema;
-
-const tripSchema = new Schema([
-  {
-    user: {
-      Type: Schema.Types.ObjectId,
-      Ref: "User"
-    },
-    title: String, //est-ce ici qu'on limite le nb de caractères?
-    description: String,
-    steps: [
-      {
-        country: String,
-        city: String,
-        startDate: Date,
-        endDate: Date
-      }
-    ]
-  }
-]);
-
-console.log("juste pour faire le commit 1");
-const tripModel = mongoose.model("Trip", tripSchema);
-
-module.exports = tripModel;
