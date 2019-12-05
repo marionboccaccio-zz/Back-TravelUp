eba86ac57cf2f14c3a4a0c800da0d98963230282
marionboccaccio <marionboccaccio@MacBook-Air-de-Marion.local>
2019-12-05 11:03:49 +0100

    trip and user models done

--- Back-TravelUp/models/Trip.js
+++ Back-TravelUp/models/Trip.js
@@ -1,2 +1,26 @@
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
+
+const tripSchema = new Schema([
+  {
+    user: {
+      Type: Schema.Types.ObjectId,
+      Ref: "User"
+    },
+    title: String, //est-ce ici qu'on limite le nb de caractères?
+    description: String,
+    steps: [
+      {
+        country: String,
+        city: String,
+        startDate: Date,
+        endDate: Date
+      }
+    ]
+  }
+]);
+
+const tripModel = mongoose.model("Trip", tripSchema);
+
+module.exports = tripModel;
--- Back-TravelUp/models/User.js
+++ Back-TravelUp/models/User.js
@@ -1,17 +1,22 @@
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema ({
-  _id:ObjectId,
+const userSchema = new Schema({
+  _id: ObjectId, // ça on ne le met pas, c'est bien ça?
   countryOfResidence: String,
   firstName: String,
   lastName: String,
   email: String,
   password: String,
-  trip: [{
-    Type : Schema.Types.ObjectId,
-    Ref : “Travel”}]
+  trip: [
+    {
+      Type: Schema.Types.ObjectId,
+      Ref: "Trip"
+    }
+  ]
+});
 
-})
+const userModel = mongoose.model("User", userSchema);
 
+module.exports = userModel;
